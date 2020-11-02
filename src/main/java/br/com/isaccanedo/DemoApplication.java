9e9280ee243d0ce0bc2e972d68d047fadcbf62f1
isaccanedo <isaccanedo@gmail.com>
2020-11-02 13:37:19 -0300

    Initial commit

---
+++ src/main/java/br/com/isaccanedo/DemoApplication.java
@@ -0,0 +1,34 @@
+package br.com.isaccanedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.web.reactive.function.server.RouterFunction;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+import reactor.core.publisher.Mono;
+
+
+@SpringBootApplication
+public class DemoApplication implements ApplicationContextInitializer<GenericApplicationContext> {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Override
+	public void initialize(GenericApplicationContext context) {
+		context.registerBean(RouterFunction.class, this::userEndpoints);
+	}
+
+	private RouterFunction<?> userEndpoints() {
+		return route(GET("/"), request -> ok().body(Mono.just("Isac"), String.class));
+	}
+}
+
+
---
+++ src/test/java/br/com/isaccanedo/DemoApplicationTests.java
@@ -0,0 +1,25 @@
+package br.com.isaccanedo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class DemoApplicationTests {
+
+	@Autowired
+	private WebTestClient client;
+
+	@Test
+	public void contextLoads() {
+		client.get().uri("/").exchange().expectBody(String.class).isEqualTo("Isac");
+	}
+
+}
